705dca1b840e8cd64afcaaad4bea233d5a964fc5
Gustavo Oliveira Garcia Ribeiro <gustavo.ribeiro@expocaccer.com.br>
2022-08-01 20:43:07 -0300

    Feat: Criando botão de interromper ciclo

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -81,7 +81,7 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
-export const StartCountDownButton = styled.button`
+export const BaseCountDownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -90,21 +90,31 @@ export const StartCountDownButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${(props) => props.theme['gray-100']};
 
   gap: 0.5rem;
   font-weight: bold;
 
   cursor: pointer;
 
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+`
+
+export const StartCountDownButton = styled(BaseCountDownButton)`
   background-color: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
 
   &:not(:disabled):hover {
     background-color: ${(props) => props.theme['green-700']};
   }
+`
 
-  &:disabled {
-    opacity: 0.7;
-    cursor: not-allowed;
+export const StopCountDownButton = styled(BaseCountDownButton)`
+  background-color: ${(props) => props.theme['red-500']};
+
+  &:hover {
+    background-color: ${(props) => props.theme['red-700']};
   }
 `
